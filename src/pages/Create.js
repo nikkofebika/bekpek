c381d7ca1f0d973414c9a29d4b9a97b0eb614ccb
nikko febika <febika.nikko@gmail.com>
2021-07-22 17:04:52 +0700

    fix header

---
+++ src/pages/Create.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const Create = () => {
+    return (
+        <Text>Create page</Text>
+    )
+}
+
+export default Create
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const Home = () => {
+    return (
+        <Text>Home page</Text>
+    )
+}
+
+export default Home
