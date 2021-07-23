88a9e7f89de8b489653a389a3e292345eeba9308
nikko febika <febika.nikko@gmail.com>
2021-07-23 17:08:45 +0700

    fix home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,31 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { ScrollView, Text, View, Box, HStack, IconButton, Icon } from "native-base"
+import Ionicon from 'react-native-ionicons'
+
+const Card = () => {
+    return (
+        <View mx={3} my={2}>
+            <Box bg="gray.50" borderTopRadius='lg' >
+                <Text bold fontSize="lg" py={1}>Extra Small</Text>
+                <Text py={2}>Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi </Text>
+            </Box>
+            <HStack alignItems="center" justifyContent="space-between" bg="gray.300" borderBottomRadius='lg'>
+                <Text>Sen, 27 Feb 2021</Text>
+                <IconButton
+                    icon={<Icon as={<Ionicon name="trash" />} color="white" />}
+                />
+            </HStack>
+        </View >
+    )
+}
 
 const Home = () => {
     return (
-        <Text>Home page</Text>
+        <ScrollView>
+            <Card />
+            <Card />
+            <Card />
+        </ScrollView>
     )
 }
 
