02c196dc23f03f551b4fe2aacd5d10d3ce40f2d1
nikko febika <febika.nikko@gmail.com>
2021-07-24 17:04:07 +0700

    add sqllite

--- src/config/routes/index.js
+++ src/config/routes/index.js
@@ -1,33 +1,41 @@
-import * as React from 'react';
-import { TouchableOpacity } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import Icon from 'react-native-ionicons'
+import React from 'react';
+import {TouchableOpacity} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import Icon from 'react-native-ionicons';
 
 import Create from '../../pages/Create';
 import Home from '../../pages/Home';
 
-
 const Stack = createStackNavigator();
 
 function Routes() {
-    return (
-        <SafeAreaProvider>
-            <NavigationContainer>
-                <Stack.Navigator initialRouteName="Home">
-                    <Stack.Screen name="Home" component={Home}
-                        options={({ navigation }) => ({
-                            title: 'BekPek',
-                            headerRight: () => (
-                                <TouchableOpacity onPress={() => navigation.navigate('Create')}><Icon ios="ios-add" android="md-add" style={{ marginRight: 15 }} /></TouchableOpacity>
-                            ),
-                        })} />
-                    <Stack.Screen name="Create" component={Create} />
-                </Stack.Navigator>
-            </NavigationContainer>
-        </SafeAreaProvider>
-    );
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={({navigation}) => ({
+              title: 'BekPek',
+              headerRight: () => (
+                <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+                  <Icon
+                    ios="ios-add"
+                    android="md-add"
+                    style={{marginRight: 15}}
+                  />
+                </TouchableOpacity>
+              ),
+            })}
+          />
+          <Stack.Screen name="Create" component={Create} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
 }
 
 export default Routes;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,59 @@
-import React from 'react'
-import { ScrollView, Text, View, Box, HStack, IconButton, Icon } from "native-base"
-import Ionicon from 'react-native-ionicons'
+import React, {useEffect, useState} from 'react';
+import {Alert, ScrollView, TouchableOpacity} from 'react-native';
+import {Text, View, Box, HStack} from 'native-base';
+import Icon from '../components/atoms/Icon';
 
 const Card = () => {
-    return (
-        <View mx={3} my={2}>
-            <Box bg="gray.50" borderTopRadius='lg' >
-                <Text bold fontSize="lg" py={1}>Extra Small</Text>
-                <Text py={2}>Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi </Text>
-            </Box>
-            <HStack alignItems="center" justifyContent="space-between" bg="gray.300" borderBottomRadius='lg'>
-                <Text>Sen, 27 Feb 2021</Text>
-                <IconButton
-                    icon={<Icon as={<Ionicon name="trash" />} color="white" />}
-                />
-            </HStack>
-        </View >
-    )
-}
+  return (
+    <View mx={3} my={2}>
+      <Box bg="gray.50" borderTopRadius="lg" px={3}>
+        <Text bold fontSize="lg" py={1}>
+          Extra Small
+        </Text>
+        <Text py={2}>
+          Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi Deskripsi
+        </Text>
+      </Box>
+      <HStack
+        alignItems="center"
+        justifyContent="space-between"
+        bg="blue.300"
+        borderBottomRadius="lg"
+        px={3}
+        py={1}>
+        <Text fontSize="sm">Sen, 27 Feb 2021</Text>
+        <HStack space={3}>
+          <Icon name="create" />
+          <Icon
+            name="trash"
+            onPress={() =>
+              Alert.alert('Alert Title', 'My Alert Msg', [
+                {
+                  text: 'Canceeleee',
+                  onPress: () => alert('cancle'),
+                },
+                {text: 'OK', onPress: () => console.log('OK Pressed')},
+              ])
+            }
+          />
+        </HStack>
+      </HStack>
+    </View>
+  );
+};
 
 const Home = () => {
+  const [datas, setDatas] = useState([]);
+  if (datas.length > 0) {
     return (
-        <ScrollView>
-            <Card />
-            <Card />
-            <Card />
-        </ScrollView>
-    )
-}
+      <ScrollView>
+        {datas.map(i => {
+          return <Card key={i.id} />;
+        })}
+      </ScrollView>
+    );
+  }
+  return <Text>No Data</Text>;
+};
 
-export default Home
+export default Home;
